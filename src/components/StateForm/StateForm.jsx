36d366eee379c7174821dd6053b5afd272c402e6
ALNoman78 <abdullaalno078@gmail.com>
2024-12-16 20:31:39 +0600

    error handle password

--- src/components/StateForm/StateForm.jsx
+++ src/components/StateForm/StateForm.jsx
@@ -2,17 +2,18 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 const StateForm = () => {
-    const [name , setName] = useState(null)
-    const [email , setEmail] = useState(null);
-    const [password , setPassword ] = useState(null);
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('')
 
     const handleUserName = e => {
-        console.log(e.target.value)
+        // console.log(e.target.value)
         setName(e.target.value)
     }
 
     const handlePassword = e => {
-        console.log(e.target.value)
+        // console.log(e.target.value)
         setPassword(e.target.value)
     }
 
@@ -23,6 +24,12 @@ const StateForm = () => {
 
     const handleFormSubmit = e => {
         e.preventDefault()
+        if (password.length < 8) {
+            setError('password must be 8 char longer')
+        } else {
+            setError()
+            console.log(name, email, password)
+        }
         console.log(name)
         console.log(email)
         console.log(password)
@@ -31,18 +38,21 @@ const StateForm = () => {
         <div>
             <form onSubmit={handleFormSubmit}>
                 <input
-                    onChange={handleUserName}
-                className=' p-3 border border-black m-3 rounded-lg' type="text " name='name' placeholder='Name'/>
+                    onChange={handleUserName} value={name}
+                    className=' p-3 border border-black m-3 rounded-lg' type="text " name='name' placeholder='Name' />
                 <br />
                 <input
                     onChange={handleEmail}
-                className=' p-3 border border-black m-3 rounded-lg' type="email" name='email' placeholder='Email'/>
+                    className=' p-3 border border-black m-3 rounded-lg' type="email" name='email' placeholder='Email' />
                 <br />
-                <input 
+                <input
                     onChange={handlePassword}
-                className=' p-3 border border-black m-3 rounded-lg' type="password" name="" id="" placeholder='Password'/>
+                    className=' p-3 border border-black m-3 rounded-lg' type="password" name="" id="" value={password} placeholder='Password' />
                 <br />
                 <input className='btn btn-success text-white ml-3' type="submit" value="Submit" />
+                {
+                    error && <p className='text-red-500 text-xl font-bold'>{error}</p>
+                }
             </form>
         </div>
     )
