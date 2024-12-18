2ab83369c212292d4130a68e009cf715ec0940ae
ALNoman78 <abdullaalno078@gmail.com>
2024-12-18 22:30:27 +0600

    custome hooks added

--- src/Hooks/useInputState.js
+++ src/Hooks/useInputState.js
@@ -3,10 +3,21 @@ import { useState } from "react"
 const useInputValue = (defaultValue = null) => {
     const [value , setValue] = useState(defaultValue)
 
-    const handleChange = val => {
-        setValue(val)
+    // this code is for object type code 
+    const onChange = (e) => {
+        setValue(e.target.value)
     }
-    return [value , handleChange]
+
+    //option like array [when i use data as a array then i should do this code]
+    // const handleChange = e => {
+    //     setValue(e.target.value)
+    // }
+
+    // return [value , handleChange]  // for array 
+    return {
+        value , 
+        onChange
+    }
 }
 
 export default useInputValue;
--- src/components/Hookform/HookForm.jsx
+++ src/components/Hookform/HookForm.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
 import PropTypes from 'prop-types'
+import useInputValue from '../../Hooks/useInputState'
 
 const HookForm = () => {
+    // const [name , handleChangeName] = useInputValue(null);
+    // const [email , handleChangeEmail] = useInputValue(null)
+    const emailState = useInputValue('default@gmail.com')
 
     const handleForm = e => {
         e.preventDefault();
-        console.log(e)
+        // console.log(name, 'from data')
+        console.log(emailState.value , 'this is form email state as a object')
     }
 
     return (
         <div className='md:w-3/12 mx-auto w-full m-3'>
             <form onSubmit={handleForm}>
                 <label className="input input-bordered flex items-center gap-2">
-                    <input type="text" className="grow" name='email' placeholder="Email" />
+                    {/* <input onChange={handleChangeName} type="text" className="grow" name='email' placeholder="Email" /> */}
                 </label>
                 <label className="input input-bordered flex items-center gap-2">
-                    <input type="text" className="grow" placeholder="Username" defaultValue={'jhonDhoe123'} />
+                    <input {...emailState} type="text" className="grow" placeholder="Username" />
                 </label>
                 <label className="input input-bordered flex items-center gap-2">
                     <input type="password" className="grow" placeholder='password' />
